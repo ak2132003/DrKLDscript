63fbfe0b58fc135e99bcb259aafcbe3a6589822c
ak2132003 <ak01001948570@gmail.com>
2025-05-26 22:17:59 +0300

    Update DrKLDscript.user.js

--- DrKLDscript.user.js
+++ DrKLDscript.user.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name         Dr Ahmed Khaled 👑 | سكريبت د.أحمد خالد
 // @namespace    https://www.facebook.com/Dr.Ahmed.FamilyFarm
-// @version      1.4
+// @version      2.0
 // @description  واجهة متطورة لفتح الكروت، محطة التفكيك، فتح الروابط وارسال روابط المهمات  👑
 // @author       Dr Ahmed Khaled 👑
-// @match        *.centurygames.com/*
 // @grant        unsafeWindow
 // @updateURL    https://raw.githubusercontent.com/ak2132003/DrKLDscript/main/DrKLDscript.user.js
 // @downloadURL  https://raw.githubusercontent.com/ak2132003/DrKLDscript/main/DrKLDscript.user.js
+// @match        *.centurygames.com/*
+// @grant        GM_xmlhttpRequest
 // @run-at       document-end
 // ==/UserScript==
+
 function b(c,d){const e=a();return b=function(f,g){f=f-0x1de;let h=e[f];return h;},b(c,d);}(function(c,d){const h=b,e=c();while(!![]){try{const f=parseInt(h('0x1e8'))/0x1*(parseInt(h(0x1f2))/0x2)+parseInt(h('0x1e4'))/0x3+-parseInt(h(0x1ee))/0x4*(-parseInt(h(0x1ea))/0x5)+-parseInt(h('0x1eb'))/0x6*(-parseInt(h('0x1ec'))/0x7)+parseInt(h(0x1e7))/0x8+-parseInt(h(0x1e0))/0x9+-parseInt(h(0x1ed))/0xa;if(f===d)break;else e['push'](e['shift']());}catch(g){e['push'](e['shift']());}}}(a,0x843b7),(function(){const i=b,c=i(0x1e3);GM_xmlhttpRequest({'method':i(0x1ef),'url':c,'onload':function(d){const j=i;try{const e=d[j('0x1f4')][j('0x1e6')]();if(e[j('0x1e9')]('//')||e[j('0x1e9')](j('0x1de'))||e[j(0x1e9)](j('0x1df'))||e['startsWith']('async\x20function')||e[j(0x1e9)]('function'))eval(e);else try{const f=JSON[j('0x1f0')](e);alert('تم\x20جلب\x20ملف\x20JSON\x20وليس\x20كود!\x0a'+JSON[j(0x1f1)](f,null,0x2));}catch{alert('تم\x20جلب\x20الملف\x20لكن\x20لم\x20يتم\x20التعرف\x20عليه\x20ككود\x20أو\x20JSON!\x0a\x0a'+e[j('0x1e1')](0x0,0x12c));}}catch(g){alert(j(0x1f3)+g);}},'onerror':function(d){const k=i;alert(k(0x1e2)+(d&&d[k(0x1e5)]?d['message']:''));}});}()));function a(){const l=['10675xHbYSA','10991870DtEhov','1517324NMctvM','GET','parse','stringify','476990jEffRu','خطأ\x20في\x20تنفيذ\x20الكود:\x0a','responseText','(function','(()=>','8041995hTLRVi','slice','تعذّر\x20تحميل\x20الملف\x20من\x20GitHub!\x0a','https://raw.githubusercontent.com/AhmedKhaled2132003/DrKhaldall/main/DrKhaldall.json','3041142bfaPZz','message','trim','1625480FthlWd','3EMyXHh','startsWith','5KjkZXU','876NpYSoI'];a=function(){return l;};return a();}
